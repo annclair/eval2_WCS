2980dedac86435df741dfdb393c6d9a53338fc42
apprenti <anneclaire.sebirou@gmail.com>
2016-11-09 11:14:03 +0100

    resolution algo voyelConsonant version longue

--- algorithmes/vowelConsonant/vowelConsonantCount.js
+++ algorithmes/vowelConsonant/vowelConsonantCount.js
@@ -5,7 +5,18 @@
 
 module.exports = function vowelConsonantCount(word){
     // YOUR CODE HERE :)
+    let v = 0;
+    let c = 0;
 
-    
-
+    if ( word && word !== '' && isNaN(word) ){
+      for (let i =0; i < word.length ; i++){
+          if (word[i].toLowerCase() == 'a' ||word[i].toLowerCase() == 'e' ||
+          word[i].toLowerCase() == 'i' || word[i].toLowerCase() == 'o' ||
+          word[i].toLowerCase() == 'u' || word[i].toLowerCase() == 'y')
+              v += 1
+          else
+              c += 1
+      }
+    }
+    return {v:v, c:c}
 }
