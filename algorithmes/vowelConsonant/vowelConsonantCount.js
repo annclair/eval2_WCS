8fe4380700ca76c73b90cfac1addbed4fc5decfe
apprenti <anneclaire.sebirou@gmail.com>
2016-11-09 11:02:16 +0100

    création du dossier

---
+++ algorithmes/vowelConsonant/vowelConsonantCount.js
@@ -0,0 +1,11 @@
+/*
+    TIPS : https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/RegExp
+    http://www.regexr.com/
+*/
+
+module.exports = function vowelConsonantCount(word){
+    // YOUR CODE HERE :)
+
+    
+
+}
