9de18a6c938ea1222e890a2a04e28f91e9662454
apprenti <anneclaire.sebirou@gmail.com>
2016-11-09 11:16:04 +0100

    resolution algo voyelConsonant regex & ternaire

--- algorithmes/vowelConsonant/vowelConsonantCount.js
+++ algorithmes/vowelConsonant/vowelConsonantCount.js
@@ -8,15 +8,8 @@ module.exports = function vowelConsonantCount(word){
     let v = 0;
     let c = 0;
 
-    if ( word && word !== '' && isNaN(word) ){
-      for (let i =0; i < word.length ; i++){
-          if (word[i].toLowerCase() == 'a' ||word[i].toLowerCase() == 'e' ||
-          word[i].toLowerCase() == 'i' || word[i].toLowerCase() == 'o' ||
-          word[i].toLowerCase() == 'u' || word[i].toLowerCase() == 'y')
-              v += 1
-          else
-              c += 1
-      }
-    }
-    return {v:v, c:c}
+    return (word && word !== '' && isNaN(word) ?
+      {v : word.toLowerCase().match(/[aeiouy]/g).length,
+      c : word.toLowerCase().match(/[^aeiouy]/g).length} :
+      {v : 0 , c : 0} )
 }
